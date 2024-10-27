be360799afea120ec1742d18b2896e2ef36b77aa
PatienceRaniah <patiencenantongo0@gmail.com>
2024-10-27 10:11:48 +0300

    fourth commit

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('login/', login_view, name='login')
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     context = {}
     return render(request, 'crypto-currency/index.html', context) 
+
+def login_view(request, *args, **kwargs):
+    context= {}
+    return render(request, 'crypto-currency/login.html', context) 
